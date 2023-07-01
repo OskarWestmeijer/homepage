f8d61155b6c48dd199f37ddb1c7367bf823a8ac3
Oskar Westmeijer <oskar.westmeijer@gmail.com>
2023-07-02 00:22:00 +0200

    feat: test public bucket

--- terraform/bucket.tf
+++ terraform/bucket.tf
@@ -6,13 +6,6 @@ resource "google_storage_bucket" "oskar_westmeijer_homepage_static_content" {
   uniform_bucket_level_access = true
 }
 
-resource "google_storage_bucket_object" "test_sample_file" {
-  name         = "sample_file.txt"
-  source       = "sample_file.txt"
-  content_type = "text/plain"
-  bucket       = google_storage_bucket.oskar_westmeijer_homepage_static_content.id
-}
-
 # Make bucket public
 resource "google_storage_bucket_iam_member" "public_content" {
   provider = google
