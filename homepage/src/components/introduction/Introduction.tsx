9171b0ca3f32d156ab7759e5b1eb9c43e7a778d2
Oskar Westmeijer <oskar.westmeijer@gmail.com>
2023-10-31 19:21:27 +0100

    add introduction section

--- homepage/src/components/introduction/Introduction.tsx
+++ homepage/src/components/introduction/Introduction.tsx
@@ -1,7 +1,22 @@
+import { Image, Text, Title, Paper, Grid } from '@mantine/core';
+import portrait from '/images/oskar_westmeijer.jpg';
+
 export function Introduction() {
     return (
-        <div className="introduction-container">
-            <p>Hello from Oskar!</p>
-        </div>
+        <Grid>
+            <Grid.Col span={{ base: 12, md: 2, lg: 2 }} />
+            <Grid.Col span={{ base: 12, md: 4, lg: 4 }}>
+                <Image radius="md" height={500} fit="contain" src={portrait} />
+            </Grid.Col>
+            <Grid.Col span={{ base: 12, md: 4, lg: 4 }}>
+                <Paper shadow="md" radius="md" withBorder p="xl">
+                    <Text>
+                        <Title order={4}>Oskar Westmeijer</Title>
+                        Hello, I work as a software developer in Hamburg, Germany! So far I have worked mostly with Java
+                        but in my free time I try out other languages and frameworks as well.
+                    </Text>
+                </Paper>
+            </Grid.Col>
+        </Grid>
     );
 }
