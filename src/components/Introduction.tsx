0c523dc333ee1ce294193102917fd91b6134122f
Oskar Westmeijer <oskar.westmeijer@gmail.com>
2023-11-07 20:39:22 +0100

    make it responsive

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -1,4 +1,4 @@
-import { Image, Text, Title, Paper, Grid, Center, Space, Group } from '@mantine/core';
+import { Image, Text, Title, Paper, Grid, Center, Space, Group, rem } from '@mantine/core';
 import portrait from '/images/oskar_westmeijer.jpg';
 import github from '/images/github.svg';
 import linkedin from '/images/linkedin.png';
@@ -8,13 +8,16 @@ const backgroundColor = '#Eff0f0';
 export function Introduction() {
     return (
         <Grid justify="center" align="center">
-            <Grid.Col span={{ base: 12, md: 2, lg: 2, xl: 2 }} />
-            <Grid.Col span={{ base: 12, md: 4, lg: 4, xl: 4 }}>
+            <Grid.Col span={{ xs: 12, sm: 12, md: 2, lg: 1, xl: 3 }} />
+            <Grid.Col span={{ xs: 12, sm: 12, md: 4, lg: 5, xl: 3 }}>
                 <Center>
-                    <Image radius="md" height={500} w="auto" fit="contain" src={portrait} />
+                    {
+                        // <Image radius="md" height={500} w="auto" fit="contain" src={portrait} />
+                    }
+                    <Image radius="md" h={500} src={portrait} style={{ maxWidth: rem(400) }} />
                 </Center>
             </Grid.Col>
-            <Grid.Col span={{ base: 12, md: 4, lg: 4, xl: 4 }}>
+            <Grid.Col span={{ xs: 12, sm: 12, md: 4, lg: 5, xl: 3 }} style={{ maxWidth: rem(450) }}>
                 <Paper bg={backgroundColor} shadow="md" radius="md" withBorder p="xl">
                     <Center>
                         <Title order={4}>Oskar Westmeijer</Title>
@@ -38,17 +41,17 @@ export function Introduction() {
                     <Center>
                         <Group>
                             <a href="https://github.com/OskarWestmeijer">
-                                <Image h="35" src={github} />
+                                <Image h={35} w="auto" fit="contain" src={github} />
                             </a>
                             <a href="https://www.linkedin.com/in/oskar-westmeijer-488449252/">
-                                <Image h="35" src={linkedin} />
+                                <Image h={35} w="auto" fit="contain" src={linkedin} />
                             </a>
                         </Group>
                     </Center>
                 </Paper>
             </Grid.Col>
 
-            <Grid.Col span={{ base: 12, md: 2, lg: 2, xl: 2 }} />
+            <Grid.Col span={{ xs: 12, sm: 12, md: 2, lg: 1, xl: 3 }} />
         </Grid>
     );
 }
