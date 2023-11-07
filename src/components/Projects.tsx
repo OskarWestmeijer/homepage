c1e53836cc600f20840e0feedebeb383aefa7d8e
Oskar Westmeijer <26967048+OskarWestmeijer@users.noreply.github.com>
2023-11-07 20:39:49 +0100

    make it responsive (#28)

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,13 +1,13 @@
-import { Badge, Button, Space, Card, Group, Text, Grid, Center, Image } from '@mantine/core';
+import { Badge, Button, Space, Card, Group, Text, Grid, Center, Image, rem } from '@mantine/core';
 import github from '/images/github.svg';
 
 const backgroundColor = '#Eff0f0';
 
 export function Projects() {
     return (
-        <Grid>
-            <Grid.Col span={{ base: 12, md: 4, lg: 4 }} />
-            <Grid.Col span={{ base: 12, md: 4, lg: 4 }}>
+        <Grid justify="center" align="center">
+            <Grid.Col span={{ base: 12, md: 4, lg: 4, xl: 4 }} />
+            <Grid.Col span={{ base: 12, md: 4, lg: 4, xl: 4 }} style={{ maxWidth: rem(500) }}>
                 <Card bg={backgroundColor} shadow="md" radius="md" withBorder>
                     <Group justify="space-between" mt="md" mb="xs">
                         <Text fw={500}>Weather-Api</Text>
@@ -25,7 +25,7 @@ export function Projects() {
                     <Center>
                         <Group>
                             <a href="https://github.com/OskarWestmeijer/weather-api">
-                                <Image height={45} src={github} />
+                                <Image height={45} w="auto" fit="contain" src={github} />
                             </a>
 
                             <a href="https://ui.oskar-westmeijer.com">
@@ -35,7 +35,7 @@ export function Projects() {
                     </Center>
                 </Card>
             </Grid.Col>
-            <Grid.Col span={{ base: 12, md: 4, lg: 4 }} />
+            <Grid.Col span={{ base: 12, md: 4, lg: 4, xl: 4 }} />
         </Grid>
     );
 }
