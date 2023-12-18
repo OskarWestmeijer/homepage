0c944e7d17d9dccdc4913ff301186f2fe699474d
Oskar Westmeijer <26967048+OskarWestmeijer@users.noreply.github.com>
2023-12-18 22:09:06 +0100

    feat: add maps project (#84)

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 ![main branch](https://github.com/OskarWestmeijer/homepage/actions/workflows/main-build-test-release.yml/badge.svg)
 [![Better Stack Badge](https://uptime.betterstack.com/status-badges/v1/monitor/vmxf.svg)](https://uptime.betterstack.com/?utm_source=status_badge)
+
+Showcasing my portfolio and other information about me and my interests.
+
 ## Technologies
 
 ```
@@ -10,25 +13,38 @@
 - Nginx
 ```
 
-## build & run
+## Build & test
 
+```bash
+npm install
+npm run build
+npm test
 ```
+
+## Local development
+
+```bash
 npm install
 npm run dev
 ```
 
-## update deps
-
-Expecting to be installed: npm install -g npm-check-updates
-
-```
-ncu
-ncu -u
-npm update --save
-```
-
 ## Prettier
 
-```
+```bash
+npm run lint
 npx prettier . --write
+```
+
+## Update dependencies
+
+```bash
+npm install -g npm-check-updates
+ncu
+
+# granular updates
+ncu -u --target=patch
+ncu -u --target=minor
+
+ncu -u
+npm update --save
 ```
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,40 +1,58 @@
-import { Badge, Button, Space, Card, Group, Text, Grid, Center, Image, rem } from '@mantine/core';
+import { Button, Stack, Card, Group, Text, Center, Image } from '@mantine/core';
 import github from '/images/github.svg';
 
 const backgroundColor = '#Eff0f0';
 
 export function Projects() {
     return (
-        <Grid justify="center" align="center">
-            <Grid.Col span={{ base: 12, md: 4, lg: 4, xl: 4 }} />
-            <Grid.Col span={{ base: 12, md: 4, lg: 4, xl: 4 }} style={{ maxWidth: rem(500) }}>
-                <Card bg={backgroundColor} shadow="md" radius="md" withBorder>
-                    <Group justify="space-between" mt="md" mb="xs">
-                        <Text fw={500}>Weather-Api</Text>
-                        <Badge variant="light" color="green">
-                            LIVE
-                        </Badge>
-                    </Group>
+        <Group justify="center">
+            <Card bg={backgroundColor} shadow="md" radius="md" withBorder maw={400}>
+                <Group justify="space-between" mt="md" mb="xs">
+                    <Text fw={500}>Weather</Text>
+                    <a href="https://github.com/OskarWestmeijer/weather" aria-label="Weather Github repository">
+                        <Image height={35} w="auto" fit="contain" src={github} alt="Github logo" />
+                    </a>
+                </Group>
+                <Stack mt={10} justify="space-between" mih={120}>
                     <Text size="sm">
-                        This API offers weather data. The API usage is documented in an OpenAPI specification. An
-                        Angular frontend presents the weather using charts.
+                        Provides weather data for selected locations. The frontend displays the weather in charts. The
+                        backend service offers a public Api.
                     </Text>
-                    <Space h={20}></Space>
 
                     <Center>
-                        <Group>
-                            <a href="https://github.com/OskarWestmeijer/weather-api" aria-label="Weather-Api Github repository">
-                                <Image height={45} w="auto" fit="contain" src={github} alt="Github logo" />
-                            </a>
-
-                            <a href="https://ui.oskar-westmeijer.com" aria-label="Weather-Api angular frontend live deployment">
-                                <Button radius="md">UI Dashboard</Button>
-                            </a>
-                        </Group>
+                        <a
+                            href="https://weather.oskar-westmeijer.com"
+                            aria-label="Weather Angular frontend live deployment"
+                        >
+                            <Button radius="md" variant="gradient">
+                                &#x26C5; Weather
+                            </Button>
+                        </a>
                     </Center>
-                </Card>
-            </Grid.Col>
-            <Grid.Col span={{ base: 12, md: 4, lg: 4, xl: 4 }} />
-        </Grid>
+                </Stack>
+            </Card>
+
+            <Card bg={backgroundColor} shadow="md" radius="md" withBorder maw={400}>
+                <Group justify="space-between" mt="md" mb="xs">
+                    <Text fw={500}>Maps</Text>
+                    <a href="https://github.com/OskarWestmeijer/maps" aria-label="Maps Github repository">
+                        <Image height={35} w="auto" fit="contain" src={github} alt="Github logo" />
+                    </a>
+                </Group>
+                <Stack mt={10} justify="space-between" mih={120}>
+                    <Text size="sm">Displays Hamburg airport connections on a 3d globe.</Text>
+                    <Center>
+                        <a
+                            href="https://maps.oskar-westmeijer.com"
+                            aria-label="Maps Sveltekit frontend live deployment"
+                        >
+                            <Button radius="md" variant="gradient" >
+                                &#127757; Maps
+                            </Button>
+                        </a>
+                    </Center>
+                </Stack>
+            </Card>
+        </Group>
     );
 }
