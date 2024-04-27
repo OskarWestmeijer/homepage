fb81493dd1d1cce2897c9b57643a03282beb862d
Oskar Westmeijer <oskar.westmeijer@gmail.com>
2024-04-27 23:46:48 +0200

    feat: init daisyui

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,12 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>Home</h1>
+  <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 </template>
+
+<style>
+.logo {
+  margin: 0 auto 2rem;
+}
+</style>
