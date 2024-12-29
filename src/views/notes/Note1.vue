e2327027d9e38ca1113b731a8b3cbcedd64abe2b
Oskar Westmeijer <26967048+OskarWestmeijer@users.noreply.github.com>
2024-12-29 22:28:22 +0100

    feat: refactor notes, add note on mutable vs immutable list (#468)

---
+++ src/views/notes/Note1.vue
@@ -0,0 +1,52 @@
+<template>
+  <div
+    class="mt-4 prose w-full lg:w-3/5 xl:w-2/5 m-auto flex flex-col items-center text-base-content"
+  >
+    <h2 class="text-center">{{ item.title }}</h2>
+    <blockquote class="border-primary mb-0">
+      <h2 class="mb-0 mt-0">Goal:</h2>
+      Prove how immutable lists do not allow changes to structure. Understand that
+      immutability of a list does not include immutability of its elements.
+    </blockquote>
+    <p>
+      Lists can be initialized as mutable or immutable. If no modifications to the list
+      are intended, it is advisable to work with a immutable list. The snippet below
+      shows, how modifying the immutable list after initialization throws an exception.
+    </p>
+    <img
+      alt="Immutable List throws exception on modification"
+      class="mt-0 min-w-xs max-w-xl rounded-xl"
+      src="@/assets/notes/note-1-1.png"
+    />
+    <blockquote class="border-primary mb-0">
+      Note! An immutable list only guarantees the structure of the list, it does not
+      prevent the possibility of its elements being mutated.
+    </blockquote>
+    <p>
+      The example below works with a simple class, which is mutable. To reduce code, I
+      used some annotations from the Lombok library.
+    </p>
+    <img
+      alt="Immutable List does not prevent its elements being mutated"
+      class="mt-0 mb-0 min-w-xs max-w-xl rounded-xl"
+      src="@/assets/notes/note-1-2.png"
+    />
+    <p>
+      While the immutable list maintains the order of elements, we were able to mutate the
+      element. In case immutability of the element is required, you can use Java
+      <code>records</code>, or Lomboks <code>@Value</code> annotation.
+    </p>
+    <p></p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      required: false,
+    },
+  },
+};
+</script>
---
+++ src/views/notes/Note2.vue
@@ -0,0 +1,62 @@
+<template>
+  <div
+    class="mt-4 prose w-full lg:w-3/5 xl:w-2/5 m-auto flex flex-col items-center text-base-content"
+  >
+    <h2 class="text-center">{{ item.title }}</h2>
+    <blockquote class="border-primary mb-0">
+      <h2 class="mb-0 mt-0">Goal:</h2>
+      Make a shallow copy of a List. Prove the independence of original & copy. Understand
+      the difference to a deep copy.
+    </blockquote>
+
+    <p>
+      Every List can be copied. A shallow copy, copies the structure (order) of the
+      original list. The original and copy are distinct objects. Subsequent modifications
+      to the original do not propagate to the copy.
+    </p>
+
+    <img
+      alt="Shallow copy"
+      class="mt-0 min-w-xs max-w-xl rounded-xl"
+      src="@/assets/notes/note-2-1.png"
+    />
+
+    <blockquote class="border-primary mb-0">
+      The difference to a deep copy is, that the elements itself are not copied. After
+      performing a shallow copy, both lists reference the same objects on the heap.
+    </blockquote>
+
+    <p>
+      If the elements are mutable, the changes propagate accross original & shallowCopy.
+      The class <code>BG2Character</code> allows mutability of the name attribute.
+    </p>
+
+    <img
+      alt="Shallow copy, with referenced element being mutated in both lists"
+      class="mt-0 min-w-xs max-w-xl rounded-xl"
+      src="@/assets/notes/note-2-2.png"
+    />
+
+    <p></p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      required: false,
+    },
+  },
+};
+</script>
+
+<style scoped>
+@media (max-width: 768px) {
+  .overflow-x-auto {
+    overflow-x: auto;
+    max-width: 100%;
+  }
+}
+</style>
